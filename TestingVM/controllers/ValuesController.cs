8974d3fdb58cf5c47c95441a307724e479bc9881
Batchu Sai Akhil <BatchuSai.Akhil@mrcooper.com>
2020-11-15 14:14:58 +0530

    temp checking

---
+++ TestingVM/controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingVM.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("addUser/{username}")]
+        public async Task<IActionResult> GetUWConditions(string username)
+        {
+
+
+            var a = "encomp_usr_" + username;
+            return new JsonResult(a);
+        }
+    }
+}
